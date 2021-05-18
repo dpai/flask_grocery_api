13094047e00baddc6fc5ef193669ac7a08e9d17b
Darshan Pai <darshanp20@yahoo.com>
2021-05-17 19:53:31 -0500

    Cleaned unused imports

--- grocery_api/app.py
+++ grocery_api/app.py
@@ -4,18 +4,9 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from flask import Flask
 from flask_restful import Api
-from grocery_api.constants import PROJECT_ROOT
-from database import GROCERY_DATABASE, db_session
-from grocery_api.models.product import Product
-from grocery_api.models.vendor import Vendor
-from grocery_api.models.shop import Shop
-from grocery_api.models.grocery import Grocery
-from grocery_api.schemas.product_schema import ProductSchema
-from grocery_api.schemas.vendor_schema import VendorSchema
-from grocery_api.schemas.shop_schema import ShopSchema
-from grocery_api.schemas.grocery_schema import GrocerySchema
+from grocery_api.database import GROCERY_DATABASE, db_session
 from grocery_api.resources.product_resource import ProductResource, PRODUCT_ENDPOINT
-from grocery_api.resources.vendor_resource import VendorResource, VENDOR_ENDPOINT
+from grocery_api.resources.vendor_resource import VendorResource, VendorByNameResource, VENDOR_ENDPOINT
 from grocery_api.resources.shop_resource import ShopResource, SHOP_ENDPOINT
 from grocery_api.resources.grocery_resource import GroceryResource, GroceryByProductNameResource, GROCERY_ENDPOINT
 
@@ -26,7 +17,8 @@ def create_app(db_location):
     api = Api(app)
 
     api.add_resource(ProductResource, PRODUCT_ENDPOINT, f"{PRODUCT_ENDPOINT}/<id>")
-    api.add_resource(VendorResource, VENDOR_ENDPOINT, f"{VENDOR_ENDPOINT}/<id>")
+    api.add_resource(VendorResource, VENDOR_ENDPOINT, f"{VENDOR_ENDPOINT}/<int:id>")
+    api.add_resource(VendorByNameResource, VENDOR_ENDPOINT, f"{VENDOR_ENDPOINT}/<string:vendor_name>")
     api.add_resource(ShopResource, SHOP_ENDPOINT, f"{SHOP_ENDPOINT}/<id>")
     api.add_resource(GroceryResource, GROCERY_ENDPOINT, f"{GROCERY_ENDPOINT}/<int:id>")
     api.add_resource(GroceryByProductNameResource, GROCERY_ENDPOINT, f"{GROCERY_ENDPOINT}/<string:name>")
@@ -36,17 +28,6 @@ def create_app(db_location):
         db_session.remove()
 
     return app
-
-#Init schema
-# product_schema = ProductSchema()
-# products_schema = ProductSchema(many=True)
-
-# # Get ALl products
-# @app.route('/product', methods=['GET'])
-# def get_products():
-#     all_products = Product.query.all()
-#     result = products_schema.dump(all_products)
-#     return jsonify(result)
 
 # Run server
 if __name__ == '__main__':
