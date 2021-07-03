2da7700773db9a6af64f2613a83898c2a658d441
Darshan Pai <darshanp20@yahoo.com>
2021-07-02 20:10:52 -0500

    Add Delete and put endpoints for shop resource

--- grocery_api/resources/shop_resource.py
+++ grocery_api/resources/shop_resource.py
@@ -44,7 +44,6 @@ class ShopByNameResource(Resource):
         #logger.info(f"Shop deleted from database {shop_json}")
         return shop_json, 200
 
-
     def _delete_shop_by_name(self, shop_name):
         shop = db_session.query(Shop).filter(Shop.shop_name==shop_name).first()
         shop_json = ShopSchema().dump(shop)
@@ -88,24 +87,24 @@ class ShopResource(Resource):
         except NoResultFound:
             abort(404, message="Shop not found")
 
-    def _get_shop_by_id(self, shop_id):
-        shop = db_session.query(Shop).filter_by(id=shop_id).first()
-        shop_json = ShopSchema().dump(shop)
-        db_session.remove()
+    def delete(self, id=None):
+        """
+        ShopByNameResource DELETE method. Delete the shop by id if found in the 
+        database. 
+        :param id: Shop id to delete
+        :return: Shop, 200 HTTP status code
+        """
 
-        if not shop_json:
-            raise NoResultFound()
+        if not id:
+            abort(405, message="Method not allowed")
 
-        #logger.info(f"Shop retrieved from database {shop_json}")
-        return shop_json
-
-    def _get_all_shops(self):
-        shops = db_session.query(Shop).all()
-        shops_json = ShopSchema(many=True).dump(shops)
-        db_session.remove()
-
-        #logger.info("Shops successfully retrieved.")
-        return shops_json
+        try:
+            shop_json = self._delete_shop_by_id(id)
+        except NoResultFound:
+            abort(404, message=f"Shop not found")
+        
+        #logger.info(f"Shop deleted from database {shop_json}")
+        return shop_json, 200
 
     def post(self):
         """
@@ -128,3 +127,66 @@ class ShopResource(Resource):
             abort(500, message="Unexpected Error!")
         else:
             return shop.id, 201
+
+    def put(self, id=None):
+        """
+        ShopResource PUT method. Updates Shop to the database.
+        :return: Shop.id, 201 HTTP status code.
+        """
+
+        if not id:
+            abort(405, message="Method not allowed")
+
+        data = request.get_json()
+
+        try:
+            shop = ShopSchema().load(data)
+        except ValidationError as e:
+            abort(500, message=e.messages)
+        
+        shop = db_session.query(Shop).filter(Shop.id==id).first()
+
+        shop.shop_name = data["shop_name"]
+        shop.location = data["location"]
+
+        try:
+            db_session.commit()
+        except IntegrityError as e:
+            # logger.warning(
+            #     f"Integrity Error, this team is already in the database. Error: {e}"
+            # )
+
+            abort(500, message="Unexpected Error!")
+        else:
+            return shop.id, 200
+
+
+    def _delete_shop_by_id(self, shop_id):
+        shop = db_session.query(Shop).filter(Shop.id==shop_id).first()
+        shop_json = ShopSchema().dump(shop)
+
+        if not shop_json:
+            raise NoResultFound()
+        
+        db_session.delete(shop)
+        db_session.commit()
+        return shop_json
+
+    def _get_shop_by_id(self, shop_id):
+        shop = db_session.query(Shop).filter_by(id=shop_id).first()
+        shop_json = ShopSchema().dump(shop)
+        db_session.remove()
+
+        if not shop_json:
+            raise NoResultFound()
+
+        #logger.info(f"Shop retrieved from database {shop_json}")
+        return shop_json
+
+    def _get_all_shops(self):
+        shops = db_session.query(Shop).all()
+        shops_json = ShopSchema(many=True).dump(shops)
+        db_session.remove()
+
+        #logger.info("Shops successfully retrieved.")
+        return shops_json
--- tests/integration/test_shop_resource.py
+++ tests/integration/test_shop_resource.py
@@ -51,9 +51,39 @@ def test_get_shop_name_not_found(client):
     assert response.status_code == 404
 
 def test_delete_one_shop(client):
-    response = client.delete(f"{SHOP_ENDPOINT}/Test3")
+    new_shop_json = {"shop_name": "Test10", "location": "WA"}
+    response = client.post(f"{SHOP_ENDPOINT}", json=new_shop_json)
+    assert response.status_code == 201
+    response = client.delete(f"{SHOP_ENDPOINT}/Test10")
     assert response.status_code == 200
 
 def test_delete_one_shop_not_found(client):
-    response = client.delete(f"{SHOP_ENDPOINT}/Test3")
+    response = client.delete(f"{SHOP_ENDPOINT}/Test10")
     assert response.status_code == 404
+
+def test_delete_one_shop_by_id(client):
+    new_shop_json = {"shop_name": "Test6", "location": "FL"}
+    response = client.post(f"{SHOP_ENDPOINT}", json=new_shop_json)
+    assert response.status_code == 201
+    response = client.delete(f"{SHOP_ENDPOINT}/{response.json}")
+    assert response.status_code == 200
+
+def test_delete_one_shop_id_not_found(client):
+    response = client.delete(f"{SHOP_ENDPOINT}/4")
+    assert response.status_code == 404
+
+def test_delete_one_shop_wrong_URI(client):
+    response = client.delete(f"{SHOP_ENDPOINT}")
+    assert response.status_code == 405
+
+def test_put_by_shop_id(client):
+    new_shop_json = {"shop_name": "Test20", "location": "PS"}
+    response = client.post(f"{SHOP_ENDPOINT}", json=new_shop_json)
+    assert response.status_code == 201
+    update_shop_json = {"shop_name": "Test21", "location": "PA"}
+    response = client.put(f"{SHOP_ENDPOINT}/{response.json}", json=update_shop_json)
+    assert response.status_code == 200
+    response = client.get(f"{SHOP_ENDPOINT}/{response.json}")
+    assert response.status_code == 200
+    assert response.json["shop_name"] == "Test21"
+    assert response.json["location"] == "PA"
