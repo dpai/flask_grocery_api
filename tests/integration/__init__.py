bac826b9507f571939aeed08c93575bb1deffa5f
Darshan Pai <darshanp20@yahoo.com>
2021-07-06 19:37:49 -0500

    Enable Foreign key constraint for sqlite3

---
+++ tests/integration/__init__.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+##https://stackoverflow.com/questions/13712381/how-to-turn-on-pragma-foreign-keys-on-in-sqlalchemy-migration-script-or-conf
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if type(dbapi_connection) is sqlite3.Connection:  # play well with other DB backends
+       cursor = dbapi_connection.cursor()
+       cursor.execute("PRAGMA foreign_keys=ON")
+       cursor.close()
