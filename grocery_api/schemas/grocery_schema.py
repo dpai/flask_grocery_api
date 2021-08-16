3abc78401161273283d40962ad3e1e647d4ef6bd
Darshan <darshan.pai@ge.com>
2021-08-15 21:28:31 -0500

    Add Post method for Grocery resource

--- grocery_api/schemas/grocery_schema.py
+++ grocery_api/schemas/grocery_schema.py
@@ -17,3 +17,15 @@ class GrocerySchema(Schema):
     def validate_fields(self, data, **kwargs):
         if (not "shop_id" in data) or (data["shop_id"] in [""]) :
             raise ValidationError({"shop_id": ["Shop field cannot be empty"]})
+        if (not "vendor_id" in data) or (data["vendor_id"] in [""]) :
+            raise ValidationError({"vendor_id": ["Vendor field cannot be empty"]})
+        if (not "product_id" in data) or (data["product_id"] in [""]) :
+            raise ValidationError({"product_id": ["Product field cannot be empty"]})
+        if (not "date_bought" in data) or (data["date_bought"] in [""]) :
+            raise ValidationError({"date_bought": ["Date Bought field cannot be empty"]})
+        if (not "weight_in_pounds" in data) or (data["weight_in_pounds"] in [""]) :
+            raise ValidationError({"weight_in_pounds": ["Weight in Pounds field cannot be empty"]})
+        if (not "quantity" in data) or (data["quantity"] in [""]) :
+            raise ValidationError({"quantity": ["Quantity field cannot be empty"]})
+        if (not "price" in data) or (data["price"] in [""]) :
+            raise ValidationError({"price": ["Price field cannot be empty"]})
--- tests/integration/test_grocery_resource.py
+++ tests/integration/test_grocery_resource.py
@@ -25,20 +25,134 @@ def test_post_one_grocery(client):
     response = client.get(f"{GROCERY_ENDPOINT}/{response.json}")
     assert response.json["date_bought"] == "2021-08-30"
 
-def test_post_one_product_empty_shop_id(client):
+def test_post_one_grocery_empty_shop_id(client):
     new_grocery_json = {"shop_id": "", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
     response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
     assert response.status_code == 500
     assert response.json["message"]["shop_id"] == ['Shop field cannot be empty']
 
-def test_post_one_product_missing_shop_id(client):
+def test_post_one_grocery_missing_shop_id(client):
     new_grocery_json = {"vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
     response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
     assert response.status_code == 500
     assert response.json["message"]["shop_id"] == ['Shop field cannot be empty']
 
-def test_post_one_product_shop_id_None(client):
+def test_post_one_grocery_shop_id_None(client):
     new_grocery_json = {"shop_id": None, "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
     response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
     assert response.status_code == 500
     assert response.json["message"]["shop_id"] == ['Field may not be null.']
+
+def test_post_one_grocery_non_exixtent_shop(client):
+    new_grocery_json = {"shop_id": "10", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"] == "Unexpected Error!"
+
+def test_post_one_grocery_missing_vendor_id(client):
+    new_grocery_json = {"shop_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["vendor_id"] == ['Vendor field cannot be empty']
+
+def test_post_one_grocery_vendor_id_None(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": None, "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["vendor_id"] == ['Field may not be null.']
+
+def test_post_one_grocery_non_exixtent_vendor(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "10", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"] == "Unexpected Error!"
+
+def test_post_one_grocery_missing_product_id(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["product_id"] == ['Product field cannot be empty']
+
+def test_post_one_grocery_product_id_None(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": None, "product_id": None, "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["product_id"] == ['Field may not be null.']
+
+def test_post_one_grocery_non_exixtent_product(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "10", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"] == "Unexpected Error!"
+
+def test_post_one_grocery_missing_date_bought(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["date_bought"] == ['Date Bought field cannot be empty']
+
+def test_post_one_grocery_date_bought_None(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": None, "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["date_bought"] == ['Field may not be null.']
+
+def test_post_one_grocery_date_bought_None(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "", "weight_in_pounds": "1", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["date_bought"] == ['Date Bought field cannot be empty']
+
+def test_post_one_grocery_missing_weight_in_pounds(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["weight_in_pounds"] == ['Weight in Pounds field cannot be empty']
+
+def test_post_one_grocery_weight_in_pounds_None(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": None, "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["weight_in_pounds"] == ['Field may not be null.']
+
+def test_post_one_grocery_weight_in_pounds_Empty(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "", "quantity": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["weight_in_pounds"] == ['Weight in Pounds field cannot be empty']
+
+def test_post_one_grocery_missing_quantity(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["quantity"] == ['Quantity field cannot be empty']
+
+def test_post_one_grocery_quantity_None(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": None, "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["quantity"] == ['Field may not be null.']
+
+def test_post_one_grocery_quantity_Empty(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "", "price": "2.99"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["quantity"] == ['Quantity field cannot be empty']
+
+def test_post_one_grocery_missing_price(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1"}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["price"] == ['Price field cannot be empty']
+
+def test_post_one_grocery_price_None(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": None}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["price"] == ['Field may not be null.']
+
+def test_post_one_grocery_price_Empty(client):
+    new_grocery_json = {"shop_id": "1", "vendor_id": "1", "product_id": "1", "date_bought": "2021-08-30", "weight_in_pounds": "1", "quantity": "1", "price": ""}
+    response = client.post(f"{GROCERY_ENDPOINT}", json=new_grocery_json)
+    assert response.status_code == 500
+    assert response.json["message"]["price"] == ['Price field cannot be empty']
